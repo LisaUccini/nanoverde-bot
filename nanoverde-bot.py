893a5a1e4d26409ccc509323607ce1561e162c22
LisaUccini <innocentiuccini.lisa@studenti-ittfedifermi.gov.it>
2018-09-10 18:45:52 +0200

    correzioni

--- nanoverde-bot.py
+++ nanoverde-bot.py
@@ -15,8 +15,9 @@ from slackclient import SlackClient
 # starterbot's user ID in Slack: value is assigned after the bot starts up
 bot_id = None
 info_user_tag = []
-events_list = [["domani alle 18 apre il nanoverde", 3, datetime.time(18, 0)], ["apertura", 4, 2]]
-
+utenti_path = "../nanoverde/utenti.txt"
+tag_path = "../nanoverde/tagpassed.txt"
+doc_path = "../nanoverde/documento.txt"
 # constants
 RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
 AWARD_COMMAND = "premio"
@@ -85,19 +86,16 @@ def verify_award(comando, event):
 
     if utente != "":
         oggi = datetime.datetime.today()
-        stoday = oggi.strftime("%y-%m-%d")
-        stoday = "20"+stoday
-        f = open("../nanoverde/documento.txt", "r")
-        leggi = f.readlines()
-        f.close()
+        stoday = oggi.strftime("%Y-%m-%d")
+        with open(doc_path, "r") as f:
+            leggi = f.readlines()
 
-        for i, val in enumerate(leggi):
+        for val in leggi:
             premio = string.split(val, ";")
             data = premio[1]
             data = string.split(data, "\n")
             data = data[0]
-            if data == stoday:
-                if utente == premio[0]:
+            if data == stoday and utente == premio[0]:
                     response = "Hai già ritirato il premio"
 
     else:
@@ -117,12 +115,12 @@ def new_user(comando, event):
     
     #open file containing the information of each user
     #example file -> code tag;name user;code slack
-    f = open("../nanoverde/utenti.txt", "r")
-    file = f.readlines()
-    f.close()
+    with open(utenti_path, "r") in f:
+        file = f.readlines()
+
     result = ""
     #verify that the user does not already exist
-    for i,var in enumerate(file):
+    for var in file:
         line = string.split(var,"\n")
         appo = line[0]
         appo = string.split(appo,";")
@@ -134,11 +132,10 @@ def new_user(comando, event):
                     return "Conosco già "+ user+" e non sei te"
             else:
                 var = line[0] + ";" + code + "\n"
-                f = open("../nanoverde/utenti.txt", "w")
-                file.append(result)
-                for i , var in enumerate(file):
-                    f.write(var)
-                f.close()
+                with open(utenti_path, "w") in f:
+                    file.append(result)
+                    for var in file:
+                        f.write(var)
                 return risposta
         else:
             if  len(appo) == 3:
@@ -167,9 +164,8 @@ def add_user_tag():
 
     global info_user_tag
     print info_user_tag
-    f = open("../nanoverde/tagpassed.txt", "r")
-    tagf = f.readlines()
-    f.close()
+    with open(tag_path, "r") in f:
+        tagf = f.readlines()
     solution = []
     find = True 
     result = "" 
@@ -178,7 +174,7 @@ def add_user_tag():
     #example file: tag code;time(%H:%M)
     text = "ERRORE: il tag non è stato passato correttamente"
     if tagf != []:
-        for i,var in enumerate(tagf):
+        for var in tagf:
             if var != "" or var != "\n":
                 tag_time = string.split(var, ";")
                 time = tag_time[1]
@@ -189,7 +185,7 @@ def add_user_tag():
 
         if len(solution) >= 5:
             result = solution[0]
-            for i , var in enumerate(solution):
+            for var in solution:
                 if result != var:
                     find=False
                     continue
@@ -202,14 +198,12 @@ def add_user_tag():
     if find:
         text = "Benvenuta " + info_user_tag[0]
         utente = result + ";" + info_user_tag[0] + ";" + info_user_tag[1] + "\n"
-        f = open("../nanoverde/utenti.txt", "r")
-        file=f.readlines()
-        f.close()
-        f = open("../nanoverde/utenti.txt", "w")
-        file.append(utente)
-        for i , var in enumerate(file):
-            f.write(var)
-        f.close()
+        with open(utenti_path, "r") in f:
+            file=f.readlines()
+        with open(utenti_path, "w") in f:
+            file.append(utente)
+            for var in file:
+                f.write(var)
 
     slack_client.api_call(
         "chat.postMessage",
@@ -222,10 +216,9 @@ def ricerca_utente(code):
     """
         search for user name from the slack code
     """
-    f = open("../nanoverde/utenti.txt", "r")
-    file_utenti = f.readlines()
-    f.close()
-    for i,val in enumerate(file_utenti):
+    with open(utenti_path, "r") in f:
+        file_utenti = f.readlines()
+    for val in file_utenti:
         user = string.split( val, ";")
         if len(user)==3:
             slack_utente = user[2]
@@ -235,27 +228,25 @@ def ricerca_utente(code):
                 return user[1]
     return ""
 
-def periodic_events():
+def periodic_events(events_list):
     """
         generates periodic events
     """
 
-    global events_list
     daynow = datetime.date.today().weekday()
     timenow = datetime.datetime.now()
     hours = timenow.hour
     minutes = timenow.minute
     seconds = timenow.second
 
-    f = open("../nanoverde/utenti.txt", "r")
-    file_utenti = f.readlines()
-    f.close()
+    with open(utenti_path, "r") in f:
+        file_utenti = f.readlines()
     text = []
 
     if hours > 9 and hours < 22:
 
         #research and analysis of events
-        for i, val in enumerate(events_list):
+        for val in events_list:
             if len(val) == 3:
                 if type(val[2]) == int:
                     if hours % val[2] == 0: #and daynow == val[1]:
@@ -267,10 +258,10 @@ def periodic_events():
                 
 
         #write events to known channels
-        for i, val in enumerate(file_utenti):
+        for val in file_utenti:
             val = string.split(val, ";")
             if len(val) == 3:
-                for j, var in enumerate(text):
+                for var in text:
                     print var
                     event_channel = string.split(val[2], "\n")
                     event_channel = event_channel[0]
@@ -284,7 +275,10 @@ def periodic_events():
 if __name__ == "__main__":
     # instantiate Slack client
     
-    slack_client = SlackClient('xoxb-430388344151-429397040834-7v98qoQ2YdPhnl0tMfOINadl')
+    #["text", number of day(monday = 0...), time or duration ]
+    events_list = [["domani alle 18 apre il nanoverde", 3, datetime.time(18, 0)], ["apertura", 4, 2]]
+    
+    slack_client = SlackClient('xoxb-430388344151-429397040834-XK6mNosxeRMa3JFZklnuteRe')
     
     if slack_client.rtm_connect(with_team_state=False):
         print("Starter Bot connected and running!")
@@ -292,8 +286,8 @@ if __name__ == "__main__":
         bot_id = slack_client.api_call("auth.test")["user_id"]
 
         while True:
-            periodic_events()
-            if not(info_user_tag == []):
+            periodic_events(events_list)
+            if info_user_tag != []:
                 if time.time() > info_user_tag[3]:
                     add_user_tag()
             command, event = parse_bot_commands(slack_client.rtm_read())
